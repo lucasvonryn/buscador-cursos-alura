ee6efe13a52d9bc398e5412bf3e4dc0ba014c70a
Lucas Von Ryn <lvonryn@gmail.com>
2026-01-12 11:56:07 -0300

    feat: requisição get ao site da alura e listagem de cursos

---
+++ buscar-cursos.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'src/Buscador.php';
+
+use GuzzleHttp\Client;
+use Alura\BuscadorDeCursos\Buscador;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client =  new Client(['base_uri' => 'https://alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
